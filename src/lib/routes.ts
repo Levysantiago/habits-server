b9081ef04b6ca850d96ba83a26eacf9d9a807169
Levy Santiago <levyssantiago@gmail.com>
2023-01-18 11:45:41 -0300

    feat: route to create habit

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,10 +1,28 @@
 import { FastifyInstance } from "fastify";
+import dayjs from "dayjs";
 import { prisma } from "./prisma";
+import { z } from "zod";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const habits = await prisma.habit.findMany();
+  app.post("/habits", async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+    const { title, weekDays } = createHabitBody.parse(request.body);
 
-    return habits;
+    const today = dayjs().startOf("day").toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: new Date(),
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return { week_day: weekDay };
+          }),
+        },
+      },
+    });
   });
 }
