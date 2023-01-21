50cf3b747fd57fafb0462152c8739477a0e0d584
Levy Santiago <levyssantiago@gmail.com>
2023-01-21 17:46:25 -0300

    fix: server host

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.register(cors);
 
 app.register(appRoutes);
 
-app.listen({ host: "localhost", port: 3333 }).then(() => {
+app.listen({ host: "0.0.0.0", port: 3333 }).then(() => {
   console.log("HTTP Server running!");
 });
